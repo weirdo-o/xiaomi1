a45993b8fc6ff03d5bf2916a02381c405f41d59e
weirdo-o <35020904+weirdo-o@users.noreply.github.com>
2018-01-31 00:37:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@
             })
         })
 
-
+// banner结束
         function xxka(a,b) {
            let nav=document.querySelectorAll(a);
            let list=document.querySelectorAll(b)
@@ -92,6 +92,166 @@
         xxka('#dapei .header_right a','#dapei .dibu1 .dibu-right')
         xxka('#peijian .header_right a','#peijian .dibu1 .dibu-right')
         xxka('#zhoubian .header_right a','#zhoubian .dibu1 .dibu-right')
-
-
+// 选项卡结束
+    function box1(box) {
+        let now=0;
+        let next=0;
+        let pic1=box.querySelectorAll('.nrul li')
+        let right1=box.querySelector('.right')
+        let left1=box.querySelector('.left')
+        let cirs1=box.querySelectorAll('.yuandian li')
+        let width=parseInt(window.getComputedStyle(box,null).width)
+        let flag=true
+        function move1() {
+            if(!flag){
+                return
+            }
+            flag=false
+            next=now+1
+            if(next>=pic1.length){
+                // next=0
+                flag=true
+            }
+            pic1[next].style.left='100%'
+            animate(pic1[now],{left:-width},400)
+            animate(pic1[next],{left:0},400,function () {
+                flag=true
+            })
+            cirs1[now].classList.remove('active')
+            cirs1[next].classList.add('active')
+            now=next
+        }
+        // let t=setInterval(move1,2000)
+        // box.onmouseenter=function () {
+        //     clearInterval(t)
+        // }
+        // box.onmouseleave=function () {
+        //     t=setInterval(move1,2000)
+        // }
+        right1.onclick=function () {
+            move1()
+        }
+        left1.onclick=function () {
+            if(!flag){
+                return
+            }
+            flag=false
+            next=now-1
+            if(next<0){
+                // next=pic1.length-1
+                flag=true
+            }
+            pic1[next].style.left='-100%'
+            animate(pic1[now],{left:width},400)
+            animate(pic1[next],{left:0},400,function () {
+                flag=true
+            })
+            cirs1[now].classList.remove('active')
+            cirs1[next].classList.add('active')
+            now=next
+        }
+        cirs1.forEach(function (val,index) {
+            val.onclick=function () {
+                if(!flag){
+                    return
+                }
+                flag=false
+                next=index
+                if(next>now){
+                    pic1[next].style.left='100%'
+                    animate(pic1[now],{left:-width},400)
+                    animate(pic1[next],{left:0},400,function () {
+                        flag=true
+                    })
+                    cirs1[now].classList.remove('active')
+                    cirs1[next].classList.add('active')
+                    now=next
+                }else if(next<now){
+                    pic1[next].style.left='-100%'
+                    animate(pic1[now],{left:width},400)
+                    animate(pic1[next],{left:0},400,function () {
+                        flag=true
+                    })
+                    cirs1[now].classList.remove('active')
+                    cirs1[next].classList.add('active')
+                    now=next
+                }else{
+                    flag=true
+                }
+            }
+        })
+    }
+        let box2=document.querySelectorAll('.nrbox')
+        box2.forEach(function (val) {
+            box1(val)
+        })
+// 底下的双下标轮播结束
+        let dibu=document.querySelector('.h-dibubox')
+        let hea=document.querySelectorAll('.h-dibubox .dibu')
+        console.log(hea)
+        let jtleft=document.querySelector('.jtleft')
+        let jtright=document.querySelector('.jtright')
+        let width=parseInt(window.getComputedStyle(dibu,null).width)
+        let now=0;
+        let next=0;
+        function move1() {
+            if(!flag){
+                return
+            }
+            flag=false
+            next=now+1
+            if(next>=hea.length){
+                next=0
+                // flag=true
+                hea[next].style.left='-100%'
+                animate(hea[now],{left:width},400)
+                animate(hea[next],{left:0},400,function () {
+                    flag=true
+                })
+                jtright.classList.remove('active')
+                jtleft.classList.add('active')
+                now=next
+            }
+            hea[next].style.left='100%'
+            animate(hea[now],{left:-width},400)
+            animate(hea[next],{left:0},400,function () {
+                flag=true
+            })
+            jtleft.classList.remove('active')
+            jtright.classList.add('active')
+            now=next
+        }
+        let t=setInterval(move1,2000)
+        dibu.onmouseenter=function () {
+            clearInterval(t)
+        }
+        dibu.onmouseleave=function () {
+            t=setInterval(move1,2000)
+        }
+        jtright.onclick=function () {
+            clearInterval(t)
+            move1()
+            jtleft.classList.remove('active')
+            jtright.classList.add('active')
+        }
+        jtleft.onclick=function () {
+            if(!flag){
+                return
+            }
+            flag=false
+            next=now-1
+            if(next<0){
+                // next=pic1.length-1
+                flag=true
+            }
+            clearInterval(t)
+            hea[next].style.left='-100%'
+            animate(hea[now],{left:width},400)
+            animate(hea[next],{left:0},400,function () {
+                flag=true
+            })
+            jtright.classList.remove('active')
+            jtleft.classList.add('active')
+            now=next
+        }
     }
